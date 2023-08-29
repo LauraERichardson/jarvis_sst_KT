74452d7f7754544b393bdaf006a9b2c06dc46682
Kisei Tanaka <kisei.tanaka@gmail.com>
2023-08-28 16:54:52 -1000

    Update the.R

--- script/the.R
+++ script/the.R
@@ -3,7 +3,7 @@ library(THE)
 
 rm(list = ls())
 
-source("C:/Users/kisei.tanaka/Jarvis_SST/script/function.R")
+source("script/function.R")
 
 sst_source = c("data/Jarvis_Sea_Surface_Temperature_CRW_Daily_1985-04-01_2023-07-31.nc",
                "data/Jarvis_Sea_Surface_Temperature_jplMUR_Daily_2002-06-01_2023-07-31.nc")[1]
@@ -23,7 +23,7 @@ sst <- stack(sst_source) %>%
 
 df = sst %>% 
   slice(-c(1, 2)) %>% 
-  filter(year %in% c(2016:2018)) %>%
+  filter(year %in% c(2010:2020)) %>%
   # filter(month %in% hot_snap$month) %>%
   select(year, month, day, starts_with("V")) %>% 
   mutate(V = rowMeans(select(., starts_with("V")), na.rm = TRUE),
